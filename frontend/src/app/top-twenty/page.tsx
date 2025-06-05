4471d23248446467e54a645bbe20faf5478f4526
Phay Someth <Phay.someth70@gmail.com>
2025-06-05 11:31:37 +0700

    commit the best practice file structure close #6

---
+++ frontend/src/app/top-twenty/page.tsx
@@ -0,0 +1,8 @@
+export default function topTwenty() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold mb-4">Welcome to the top 20 page</h1>
+      <p>This is where the top 20 interface will be.</p>
+    </main>
+  )
+}
